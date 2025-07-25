7a22be3217bc947dd2b4d0aace50acb331b16d1e
jzyoung <jiangzyang@gmail.com>
2025-07-25 12:03:41 +0800

    remove unused file

--- assignment1-basics/cs336_basics/test.py
+++
@@ -1,6 +0,0 @@
-def decode_utf8_bytes_to_str_wrong(bytestring: bytes):
-    return bytestring.decode("utf-8")
-    # return "".join([bytes([b]).decode("utf-8") for b in bytestring])
-
-print(decode_utf8_bytes_to_str_wrong("hello".encode("utf-8")))
-print("hello".encode("utf-8"))
